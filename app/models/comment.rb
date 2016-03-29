2b4b8d456582fc908fed1789cba72ea0fcf88444
alexgonchar <alegoncharuk@gmail.com>
2016-03-29 09:42:46 +0000

    Add validation check_saturday and test of this case

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,8 +1,14 @@
 class Comment < ActiveRecord::Base
   belongs_to :article
   validate :check_bad_words
+  validate :check_saturday
 
   def check_bad_words
     errors.add(:body, 'dirty') if body.include?('faq')
   end
+    
+  def check_saturday
+    errors.add(:base, 'Today St!') if Date.today.saturday?
+  end
+ 
 end
--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -3,8 +3,10 @@ require 'rails_helper'
 RSpec.describe Comment, type: :model do
   it "doesn't valid when test contain bad word" do
     comment = build :comment, body: 'Blah blah faq'
-    comment.save
+    Timecop.freeze(Date.local(2016, 03, 19)) do
+      comment.save
     # comment.valid?.should be false
-    expect(comment.errors[:body]).to eq ['dirty']
+      expect(:base, 'Today St!') if Date.today.saturday?
+    end
   end
 end
